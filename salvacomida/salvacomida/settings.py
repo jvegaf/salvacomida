91ac351cc9383ae13c79225c62cc2520a76eabad
Jose Vega <josevega234@gmail.com>
2023-04-06 00:06:39 +0100

    change db

--- salvacomida/salvacomida/settings.py
+++ salvacomida/salvacomida/settings.py
@@ -126,7 +126,7 @@ WSGI_APPLICATION = 'salvacomida.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
         'USER': '',
         'PASSWORD': '',
         'HOST': '',
